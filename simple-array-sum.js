f32ef08c7c9138e483d92a7236c2cbc682f3f2bd
Sascha Kiefer <sascha.kiefer@mail.de>
2019-12-11 22:52:50 +0100

    solved simple-array-sum with 10/10 points

---
+++ simple-array-sum.js
@@ -0,0 +1,8 @@
+//https://www.hackerrank.com/challenges/simple-array-sum/problem?h_r=profile
+
+/*
+ * Complete the simpleArraySum function below.
+ */
+function simpleArraySum(ar) {
+    return ar.reduce((a,b) => a+b,0);
+ }
