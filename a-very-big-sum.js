f443559aa120b64258d3103b7d1ecc442eec783e
Sascha Kiefer <sascha.kiefer@mail.de>
2019-12-11 22:47:00 +0100

    solved a-very-big-sum with 10/10 points

---
+++ a-very-big-sum.js
@@ -0,0 +1,5 @@
+//https://www.hackerrank.com/challenges/a-very-big-sum/problem?h_r=profile
+// Complete the aVeryBigSum function below.
+function aVeryBigSum(ar) {
+    return ar.reduce((a, b) => a + b, 0)
+   }
