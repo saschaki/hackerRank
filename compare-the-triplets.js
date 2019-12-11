ac570785cd32a3df6d2e296a5089ca760835185f
Sascha Kiefer <sascha.kiefer@mail.de>
2019-12-11 22:50:12 +0100

    solved compare-the-triplets with 10/10 points

---
+++ compare-the-triplets.js
@@ -0,0 +1,10 @@
+//https://www.hackerrank.com/challenges/compare-the-triplets/problem?h_r=profile
+
+// Complete the compareTriplets function below.
+function compareTriplets(a, b) {
+    let outcome = [0,0];
+    for(var i =0; i<=2;i++){
+        a[i]!=b[i] && (a[i] >b[i] ? outcome[0]++ : outcome[1]++);    
+    }
+    return outcome;
+    }
