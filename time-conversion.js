f3106c73587fa266f81847e14e4d05925c950668
Sascha Kiefer <sascha.kiefer@mail.de>
2019-12-12 14:40:27 +0100

    solved time-conversion with 15/15 points

---
+++ time-conversion.js
@@ -0,0 +1,17 @@
+//https://www.hackerrank.com/challenges/time-conversion/problem
+
+/*
+ * Complete the timeConversion function below.
+ */
+
+
+function timeConversion(s) {
+    /*
+     * Write your code here.
+     */
+const arr = s.slice(0,8).split(':');
+arr[0] = (s.indexOf('PM') > -1) ?
+         (arr[0] == 12 ? '12' : Number(arr[0]) + 12) :
+         (arr[0] == 12 ? '00' : arr[0]);
+return arr.join(':');
+}
