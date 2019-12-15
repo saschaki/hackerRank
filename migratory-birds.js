d16b365e7ad69d9909e3265d1fa1f9746df31d62
Sascha Kiefer <sascha.kiefer@mail.de>
2019-12-16 00:16:24 +0100

    Cleaner Code via variable renaming

--- migratory-birds.js
+++ migratory-birds.js
@@ -3,9 +3,9 @@
 // Complete the migratoryBirds function below.
 function migratoryBirds(arr) {
     const map = arr.reduce((acc, e) => acc.set(e, (acc.get(e) || 0) + 1), new Map()); 
-     let maxValue = Math.max(...[...map.values()]);
-     let maxKeys = [...map.entries()]
-        .filter(({ 1: v }) => v === maxValue)
-        .map(([k]) => k)
-        return Math.min(...maxKeys);
-}
+      let maxValue = Math.max(...[...map.values()]);
+      let keysWithMaxValue = [...map.entries()]
+         .filter(({ 1: v }) => v === maxValue)
+         .map(([k]) => k)
+         return Math.min(...keysWithMaxValue);
+ }
