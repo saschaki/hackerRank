8723c800a6f0e1e4ef2b6e629af99960d0206888
Sascha Kiefer <sascha.kiefer@mail.de>
2019-12-17 16:20:09 +0100

    solved counting-valleys with 15/15 points

---
+++ counting-valleys.js
@@ -0,0 +1,23 @@
+//https://www.hackerrank.com/challenges/counting-valleys/problem?h_r=next-challenge&h_v=zen
+
+function countingValleys(n, s) {
+//n number of steps, s = path string
+let valleyCount = 0;
+let valley = false;
+let pathArray = [...s];
+let level = 0;
+for (let i =0;i< n;i++){
+   pathArray[i] === 'D' ? level-- : level++;
+   if(level <0 && valley === false){
+       valley = true;
+   }
+   if(level ===0 && valley == true){
+       valleyCount++;
+       valley = false;
+   }
+}
+console.log(valleyCount);
+return valleyCount;
+}
+
+
